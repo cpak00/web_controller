0705f47cfcd9063ca127ef73b0aa5a54dcac89c2
cpak00 <chenyiming123@126.com>
2017-07-04 11:48:58 +0800

    cpak00

--- web_server.py
+++ web_server.py
@@ -2,29 +2,31 @@
 # this python code use decode function, which must be used in python3.x
 from bottle import get, post, run, request, template, route, static_file
 from config import IP_ADDRESS, PORT, end, control
+import os
 
+currentPath = os.path.dirname(os.path.realpath(__file__))
 
 @route('/static/css/<filename>')
 def server_static(filename):
     # currentPath = os.path.dirname(os.path.realpath(__file__))
-    return static_file(filename, root='./static/css')
+    return static_file(filename, root=(currentPath+'/static/css'))
 
 
 @route('/static/fonts/<filename>')
 def server_static(filename):
     # currentPath = os.path.dirname(os.path.realpath(__file__))
-    return static_file(filename, root='./static/fonts')
+    return static_file(filename, root=(currentPath+'/static/fonts'))
 
 
 @route('/static/js/<filename>')
 def server_static(filename):
     # currentPath = os.path.dirname(os.path.realpath(__file__))
-    return static_file(filename, root='./static/js')
+    return static_file(filename, root=(currentPath+'/static/js'))
 
 
 @get("/")
 def index():
-    return template("index")
+    return template(currentPath+r"/index")
 
 
 @post("/cmd")
